83ecf67ab8210841aafab025cc357e19efb79230
BouchraBenlamkadem <bouchra.benlamkadem@gmail.com>
2021-09-13 23:48:14 +0100

    create slice file and export it

--- helpers.js
+++ helpers.js
@@ -1,8 +1,10 @@
-module.exports = { 
-  ifIn : function (list,property,element, options) {
-    return list.some(item => item[property] == element )?  options.fn(this):options.inverse(this);
+module.exports = {
+  ifIn: function (list, property, element, options) {
+    return list.some((item) => item[property] == element)
+      ? options.fn(this)
+      : options.inverse(this);
   },
-  ifEquals : function (first,second, options) {
-      return first == second ?  options.fn(this):options.inverse(this);
+  ifEquals: function (first, second, options) {
+    return first == second ? options.fn(this) : options.inverse(this);
   },
-}
+};
--- plopfile.js
+++ plopfile.js
@@ -1,79 +1,111 @@
- const helpers = require( "./helpers");
-  const recursive = require('inquirer-recursive');
+const helpers = require("./helpers");
+const recursive = require("inquirer-recursive");
 
 module.exports = function (plop) {
-
-  plop.setPrompt('recursive', recursive);
+  plop.setPrompt("recursive", recursive);
   for (const helper in helpers) {
     plop.setHelper(helper, helpers[helper]);
   }
-  plop.setGenerator('screen', {
-      description: 'React component using Typescript',
-      prompts: [
+  plop.setGenerator("screen", {
+    description: "React component using Typescript",
+    prompts: [
+      {
+        type: "input",
+        name: "path",
+        message: "Src path: ",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Name: ",
+      },
+      {
+        type: "recursive",
+        message: "Add a new prop ?",
+        name: "props",
+        prompts: [
+          {
+            type: "list",
+            name: "source",
+            message: "What is the prop source ?",
+            choices: () => [
+              {
+                name: "Add the prop from route",
+                value: "route",
+              },
+              {
+                name: "Add the prop from state",
+                value: "state",
+              },
+            ],
+          },
+          {
+            when: (answer) => answer.source === "state",
+            type: "confirm",
+            name: "createNewSlice",
+            message: "Create new Slice ?",
+          },
+          {
+            when: (answer) => answer.source === "state",
+            type: "input",
+            name: "slice",
+            message: "Slice: ",
+          },
+          {
+            type: "input",
+            name: "name",
+            message: "Prop name: ",
+          },
+          {
+            when: (answer) => answer.createNewSlice,
+            type: "input",
+            name: "initial",
+            message: "Prop initial value: ",
+          },
+        ],
+      },
+    ],
+    actions: function (data) {
+      let actions = [
         {
-          type: 'input',
-          name: 'path',
-          message: 'Src path: ',
-        },{
-          type: 'input',
-          name: 'name',
-          message: 'Name: ',
+          type: "add",
+          path: "{{path}}/templates/{{properCase name}}Template.js",
+          templateFile: "plop_templates/screen/template.js.hbs",
         },
         {
-          type: 'recursive',
-          message: 'Add a new prop ?',
-          name: 'props',
-          prompts: [
-            {
-              type: 'list',
-              name: 'source',
-              message: 'What is the prop source ?',
-              choices: () => [
-                {
-                  name: "Add the prop from route",
-                  value: "route"
-                },
-                {
-                  name: "Add the prop from state",
-                  value: "state"
-                }
-              ]
-            },
-            {
-              type: 'input',
-              name: 'name',
-              message: 'Prop name: ',
-            },
-            {
-              when: answer => answer.source === "state",
-              type: 'input',
-              name: 'slice',
-              message: 'Slice: ',
-            }
-          ]
-      }
-      ],
-      actions: [
-        {
-          type: 'add',
-          path: '{{path}}/templates/{{properCase name}}Template.js',
-          templateFile: 'plop_templates/screen/template.js.hbs',
+          type: "append",
+          path: "{{path}}/templates/index.js",
+          template:
+            'export {default as {{ properCase name }}Template, } from "./{{ properCase name }}Template";',
         },
         {
-          type: 'append',
-          path: '{{path}}/templates/index.js',
-          template: 'export {default as {{ properCase name }}Template, } from "./{{ properCase name }}Template";'
+          type: "add",
+          path: "{{path}}/screens/{{properCase name}}.js",
+          templateFile: "plop_templates/screen/screen.js.hbs",
         },
         {
-          type: 'add',
-          path: '{{path}}/screens/{{properCase name}}.js',
-          templateFile: 'plop_templates/screen/screen.js.hbs',
+          type: "append",
+          path: "{{path}}/screens/index.js",
+          template:
+            'export {default as {{ properCase name }}, } from "./{{ properCase name }}";',
         },
-        {
-          type: 'append',
-          path: '{{path}}/screens/index.js',
-          template: 'export {default as {{ properCase name }}, } from "./{{ properCase name }}";'
+      ];
+      data.props.forEach((prop) => {
+        if (prop.createNewSlice) {
+          actions.push({
+            type: "add",
+            path: `{{path}}/slices/${prop.slice}Slice.js`,
+            templateFile: "plop_templates/screen/slice.js.hbs",
+          });
+          actions.push({
+            type: "append",
+            path: "{{path}}/slices/index.js",
+            template: `export {default as ${prop.slice}Slice, } from "./${prop.slice}Slice";`,
+          });
         }
-      ]
+      });
+
+      return actions;
+    },
   });
 };
